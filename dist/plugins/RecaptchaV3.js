30d14b95a475a267c56e9f4e4fe21e2e95d83787
vladimir-xaro <vladimir.xaro@gmail.com>
2021-06-15 18:54:53 +0300

    next fix

--- dist/plugins/RecaptchaV3.js
+++ dist/plugins/RecaptchaV3.js
@@ -1,43 +1,45 @@
-export default class {
-  constructor() {
-    this.g = grecaptcha, this._name = "RecaptchaV3", this._ready = !1, this._class = void 0, 
-    this._loaded = !1, this._queue = [];
+class RecaptchaV3 {
+  static _runQueue() {
+    for (const form of RecaptchaV3._queue) RecaptchaV3._generateTokens(form);
   }
-  _runQueue() {
-    for (const form of this._queue) this._generateTokens(form);
-  }
-  _onReady(cb) {
-    this.g.ready((() => {
-      this._ready = !0, cb();
+  static _onReady(cb) {
+    RecaptchaV3.g.ready((() => {
+      RecaptchaV3._ready = !0, cb();
     }));
   }
-  _execute(form) {
-    this.g.execute(this._class.config.recaptcha_site, {
+  static _execute(form) {
+    RecaptchaV3.g.execute(RecaptchaV3._class.config.recaptcha_site, {
       action: form.config.recaptcha_action
     }).then((token => {
-      form.plugins.config[this._name].input.value = token, form.unlockBtns();
+      form.plugins.config[RecaptchaV3._name].input.value = token, form.unlockBtns();
     }));
   }
-  _generateTokens(form) {
-    this._ready ? this._execute(form) : this._onReady((() => this._execute(form)));
+  static _generateTokens(form) {
+    RecaptchaV3._ready ? RecaptchaV3._execute(form) : RecaptchaV3._onReady((() => RecaptchaV3._execute(form)));
   }
-  init(form) {
-    if (this._class || (this._class = form.constructor), 0 === this._class.numbers) if (this.g) this.g = window.grecaptcha, 
-    this._loaded = !0; else {
+  static init(form) {
+    if (RecaptchaV3._class || (RecaptchaV3._class = form.constructor), 0 === RecaptchaV3._class.numbers) if (RecaptchaV3.g) RecaptchaV3.g = window.grecaptcha, 
+    RecaptchaV3._loaded = !0; else {
       const lib = document.createElement("script");
-      lib.src = "https://www.google.com/recaptcha/api.js?render=" + this._class.config.recaptcha_site, 
+      lib.src = "https://www.google.com/recaptcha/api.js?render=" + RecaptchaV3._class.config.recaptcha_site, 
       document.head.append(lib), lib.onload = () => {
-        this.g = window.grecaptcha, this._loaded = !0, this._runQueue(), this._queue = [];
+        RecaptchaV3.g = window.grecaptcha, RecaptchaV3._loaded = !0, RecaptchaV3._runQueue(), 
+        RecaptchaV3._queue = [];
       };
     }
     const input = document.createElement("input");
     input.setAttribute("type", "hidden"), input.setAttribute("name", "g-recaptcha-response"), 
-    input.setAttribute("id", "g-recaptcha-" + this._class.numbers), form.plugins.config[this._name] = {
+    input.setAttribute("id", "g-recaptcha-" + RecaptchaV3._class.numbers), form.plugins.config[RecaptchaV3._name] = {
       input: input
-    }, form.config.el.append(input), this._loaded ? this._generateTokens(form) : this._queue.push(form);
+    }, form.config.el.append(input), RecaptchaV3._loaded ? RecaptchaV3._generateTokens(form) : RecaptchaV3._queue.push(form);
   }
-  afterSubmit(form, success, errors, side) {
-    "client" !== side && this._generateTokens(form);
+  static afterSubmit(form, success, errors, side) {
+    "client" !== side && RecaptchaV3._generateTokens(form);
   }
 }
+
+RecaptchaV3.g = grecaptcha, RecaptchaV3._name = "RecaptchaV3", RecaptchaV3._ready = !1, 
+RecaptchaV3._class = void 0, RecaptchaV3._loaded = !1, RecaptchaV3._queue = [];
+
+export default RecaptchaV3;
 //# sourceMappingURL=RecaptchaV3.js.map
--- src/plugins/RecaptchaV3.ts
+++ src/plugins/RecaptchaV3.ts
@@ -1,83 +1,83 @@
 export default class RecaptchaV3 {
-  public g        = grecaptcha;
+  public static g        = grecaptcha;
 
-  private _name   ='RecaptchaV3';
-  private _ready  = false;
-  private _class  = undefined;
-  private _loaded = false;
-  private _queue  = [];
+  private static _name   ='RecaptchaV3';
+  private static _ready  = false;
+  private static _class  = undefined;
+  private static _loaded = false;
+  private static _queue  = [];
 
-  private _runQueue() {
-    for (const form of this._queue) {
-      this._generateTokens(form);
+  private static _runQueue() {
+    for (const form of RecaptchaV3._queue) {
+      RecaptchaV3._generateTokens(form);
     }
   };
-  private _onReady(cb) {
-    this.g.ready(() => {
-      this._ready = true;
+  private static _onReady(cb) {
+    RecaptchaV3.g.ready(() => {
+      RecaptchaV3._ready = true;
       cb();
     });
   };
 
-  private _execute(form) {
-    this.g.execute(this._class.config['recaptcha_site'], { action: form.config.recaptcha_action })
+  private static _execute(form) {
+    RecaptchaV3.g.execute(RecaptchaV3._class.config['recaptcha_site'], { action: form.config.recaptcha_action })
     .then(token => {
-      form.plugins.config[this._name].input.value = token;
+      form.plugins.config[RecaptchaV3._name].input.value = token;
       form.unlockBtns();
     });
   };
 
-  private _generateTokens(form) {
-    if (this._ready) {
-      this._execute(form);
+  private static _generateTokens(form) {
+    if (RecaptchaV3._ready) {
+      RecaptchaV3._execute(form);
     } else {
-      this._onReady(() => this._execute(form));
+      RecaptchaV3._onReady(() => RecaptchaV3._execute(form));
     }
   };
 
-  public init(form) {
-    if (! this._class) {
-      this._class = form.constructor;
+  public static init(form) {
+    if (! RecaptchaV3._class) {
+      RecaptchaV3._class = form.constructor;
     }
 
-    if (this._class.numbers === 0) {
-      if (! this.g) {
+    if (RecaptchaV3._class.numbers === 0) {
+      if (! RecaptchaV3.g) {
         const lib = document.createElement('script');
-        lib.src = 'https://www.google.com/recaptcha/api.js?render=' + this._class.config['recaptcha_site'];
+        lib.src = 'https://www.google.com/recaptcha/api.js?render=' + RecaptchaV3._class.config['recaptcha_site'];
         document.head.append(lib);
         lib.onload = () => {
-          this.g = (window as any).grecaptcha;
-          this._loaded = true;
-          this._runQueue();
-          this._queue = [];
+          RecaptchaV3.g = (window as any).grecaptcha;
+          RecaptchaV3._loaded = true;
+          RecaptchaV3._runQueue();
+          RecaptchaV3._queue = [];
         }
       } else {
-        this.g = (window as any).grecaptcha;
-        this._loaded = true;
+        RecaptchaV3.g = (window as any).grecaptcha;
+        RecaptchaV3._loaded = true;
       }
     }
 
     const input = document.createElement('input');
     input.setAttribute('type', 'hidden');
     input.setAttribute('name', 'g-recaptcha-response');
-    input.setAttribute('id', 'g-recaptcha-' + this._class.numbers);
-    form.plugins.config[this._name] = {
+    input.setAttribute('id', 'g-recaptcha-' + RecaptchaV3._class.numbers);
+    form.plugins.config[RecaptchaV3._name] = {
       input,
     }
     form.config.el.append(input);
 
-    if (this._loaded) {
-      this._generateTokens(form);
+    if (RecaptchaV3._loaded) {
+      RecaptchaV3._generateTokens(form);
     } else {
-      this._queue.push(form);
+      RecaptchaV3._queue.push(form);
     }
   };
 
-  public afterSubmit(form, success, errors, side) {
+  public static afterSubmit(form, success, errors, side) {
     if (side === 'client') {
       return;
     }
 
-    this._generateTokens(form);
+    RecaptchaV3._generateTokens(form);
   }
 }
